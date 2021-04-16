a19aca420ce01406ada0b61986ffba9a23bc873c
PilarMontes <piliimcz@gmail.com>
2021-04-15 21:57:10 -0500

    practice

---
+++ fundamentals/operadores.js
@@ -0,0 +1,71 @@
+//var a = 5;
+//var b = 10;
+//var c = 15;
+// Comentarios sencillos (1 linea) 
+/**
+ * Comentarios multilinea
+ * Ejemplo
+ * 
+ */
+
+// // Multiplicación
+// a * b // 50
+// console.log(a * b)
+
+// // Adición
+// a + c // 20
+// console.log(a + c)
+
+// // División
+// b / a // 2
+// console.log(b / a)
+
+// // Resta
+// c - a // 10
+// // console.log(c - a)
+
+// // Modulo (Arroja el residuo de una división)
+// b % a // 0
+// // console.log(b % a)
+
+
+// Incremento o decremento.
+//c++; // 16
+//console.log(c) // 16
+//c++;
+//console.log(c) // 17
+
+//b-- // 9
+//console.log(b) // 9
+
+
+/**
+ * Ejercicio 1
+ * 1.- Declarar 4 variables
+ * 2.- Hacer 2 operaciones con cada uno de los operadores antes vistos
+ * 
+ */
+var x = 10;
+var y = 20;
+var w = 2;
+var z = 100;
+
+console.log (x*z)
+console.log (w+z)
+console.log (y-w)
+console.log (z%w)
+console.log (z/w)
+console.log (x/y)
+console.log (y*z)
+console.log (x+y)
+console.log (z-y)
+console.log (x%y)
+
+y++
+console.log (y)
+
+z--
+console.log (z)
+
+
+
